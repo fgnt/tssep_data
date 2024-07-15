f5640023ffced01e8f59b194df6e9f203ce0e63a
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2024-07-15 13:20:32 +0200

    add constants file

---
+++ tssep_data/data/constants.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import tssep_data
+
+egs_dir = tssep_data.git_root / 'egs'
+eg_dir = egs_dir / 'libri_css'
+json_dir = eg_dir / 'data/jsons'
+
+if not json_dir.exists():
+    for p in [
+        '/scratch/hpc-prf-nt1/cbj/deploy/tssep_data/egs/libri_css/data/jsons',
+        '/net/vol_old/boeddeker/deploy/tssep_data/egs/libri_css/data/jsons',
+        '/scratch/hpc-prf-nt2/cbj/deploy/tssep_data/egs/libri_css/data/jsons',
+        '/scratch-n1/hpc-prf-nt1/cbj/deploy/tssep_data/egs/libri_css/data/jsons',
+        '/scratch-n2/hpc-prf-nt2/cbj/deploy/tssep_data/egs/libri_css/data/jsons',
+    ]:
+        p = Path(p)
+        if p.exists():
+            json_dir = p
+            break
